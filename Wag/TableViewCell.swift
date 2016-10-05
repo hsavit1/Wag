c5b9335dbcd1d8a9971301104ab46e5de8c0bd1d
Henry Savit <henry.savit@ambulnz.com>
2016-10-05 13:52:47 -0400

    initial commit

---
+++ Wag/TableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TableViewCell.swift
+//  Wag
+//
+//  Created by Henry Savit on 10/4/16.
+//  Copyright © 2016 HenrySavit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewCell : UITableViewCell {
+    
+    @IBOutlet weak var gravitarImageView: UIImageView!
+    @IBOutlet weak var username: UILabel!
+    
+    @IBOutlet weak var goldMedals: UILabel!
+    @IBOutlet weak var silverMedals: UILabel!
+    @IBOutlet weak var bronzeMedals: UILabel!
+    
+    
+}
